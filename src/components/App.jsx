92c2a6110eb231373e829d9769968af5e22f65fb
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2024-02-09 15:21:42 +0200

    new-comit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+
+import { Profile } from './Profile';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
