b4ff3006f16a9ccdac34437bbd8a13ee0c5b354e
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2024-02-09 21:34:50 +0200

    update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,23 +10,23 @@ export const Profile = ({ username, tag, location, image, stats }) => {
                 src={image}
                 alt="User avatar"
               />      
-            <p>{username}</p>
-            <p>@{tag}</p>
-            <p>{location}</p>
+            <p className="profile-username">{username}</p>
+            <p className="profile-tag">@{tag}</p>
+            <p className="profile-location">{location}</p>
         </div>
 
           <ul className= "status">
-            <li>
-              <span>Followers</span>
-              <span>{stats.followers}</span>
+            <li className="profile-status">
+              <span className="profile-status-h1">Followers</span>
+              <span className="profile-status-h2">{stats.followers}</span>
             </li>
-            <li>
-              <span>Views</span>
-              <span>{stats.views}</span>
+            <li className="profile-status">
+              <span className="profile-status-h1">Views</span>
+              <span className="profile-status-h2">{stats.views}</span>
             </li>
-            <li>
-              <span>Likes</span>
-              <span>{stats.likes}</span>
+            <li className="profile-status">
+              <span className="profile-status-h1">Likes</span>
+              <span className="profile-status-h2">{stats.likes}</span>
             </li>
           </ul>
         
