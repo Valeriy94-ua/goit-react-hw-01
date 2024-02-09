2ccd214b91f4189304f30a930b47cb5b8880255a
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2024-02-09 21:36:13 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,6 @@ const userData = {
   tag: "jgluke",
   location: "Ocho Rios, Jamaica",
   avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  username: "Jacques Gluke",
   stats: {
     followers: 5603,
     views: 4827,
